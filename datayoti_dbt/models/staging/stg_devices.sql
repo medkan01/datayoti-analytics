c666afcb64c9007343553d875b6eaeae69564bb7
medkan01 <mehdiakniou@gmail.com>
2025-10-28 21:06:11 +0100

    feat: Rename columns in staging models for consistency and clarity

--- datayoti_dbt/models/staging/stg_devices.sql
+++ datayoti_dbt/models/staging/stg_devices.sql
@@ -1,12 +1,10 @@
 select
-    cast(id as integer) as device_id,
-    cast(device_id as varchar(17)) as mac_addr, -- Format d'adresse MAC (XX:XX:XX:XX:XX:XX)
+    cast(id as integer) as device_source_id,
+    cast(device_id as varchar(17)) as device_mac_addr, -- Format d'adresse MAC (XX:XX:XX:XX:XX:XX)
     cast(site_id as varchar(8)) as site_ref,
     cast(created_at as timestamp) as created_ts,
     cast(updated_at as timestamp) as updated_ts
-from 
-    {{ source('raw_iot', 'raw_devices') }}
-where
-    id is not null
-    and device_id is not null
-    and site_id is not null
+from {{ source('raw_iot', 'raw_devices') }}
+where id is not null
+and device_id is not null
+and site_id is not null
