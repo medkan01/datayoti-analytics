52f315a26b141e664eeaf4c95db8b54e6c67b8a3
medkan01 <mehdiakniou@gmail.com>
2025-10-28 21:06:28 +0100

    feat: Update column names for device and site references in intermediate model for clarity

--- datayoti_dbt/models/intermediate/int_devices.sql
+++ datayoti_dbt/models/intermediate/int_devices.sql
@@ -1,16 +1,13 @@
 select
-    d.device_id as device_source_id,
-    d.mac_addr,
-    s.site_id as site_source_id,
-    d.created_ts as first_seen_ts,
-    d.updated_ts as last_seen_ts
-from
-    {{ ref('stg_devices') }} as d
-join
-    {{ ref('stg_sites') }} as s
+    d.device_source_id,
+    d.device_mac_addr,
+    s.site_source_id,
+    d.created_ts as created_ts,
+    d.updated_ts as updated_ts
+from {{ ref('stg_devices') }} as d
+left join {{ ref('stg_sites') }} as s
     on d.site_ref = s.site_ref
-where
-    s.site_id is not null
-    and s.site_ref is not null
-    and d.site_ref is not null
+where s.site_source_id is not null
+and s.site_ref is not null
+and d.site_ref is not null
     
