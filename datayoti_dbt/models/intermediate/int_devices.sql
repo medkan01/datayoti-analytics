edc98a755a0ca388d5d0718c517e415ef454c7f0
medkan01 <mehdiakniou@gmail.com>
2025-10-29 20:07:01 +0100

    feat: Simplify filtering in int_devices SQL by consolidating where conditions

--- datayoti_dbt/models/intermediate/int_devices.sql
+++ datayoti_dbt/models/intermediate/int_devices.sql
@@ -7,7 +7,4 @@ select
 from {{ ref('stg_devices') }} as d
 left join {{ ref('stg_sites') }} as s
     on d.site_ref = s.site_ref
-where s.site_source_id is not null
-and s.site_ref is not null
-and d.site_ref is not null
-    
+where d.site_ref is not null
