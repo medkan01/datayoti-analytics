a4f62c5ddd3f131b0be13bbc2aab28f40bd5ef2b
medkan01 <mehdiakniou@gmail.com>
2025-10-30 01:19:25 +0100

    fix: Update column references from device_id and site_id to device_mac_addr and site_ref in raw_devices model

--- datayoti_dbt/models/raw/raw_devices.sql
+++ datayoti_dbt/models/raw/raw_devices.sql
@@ -2,8 +2,8 @@
 
 select
     cast(null as text) as id,
-    cast(null as text) as device_id,
-    cast(null as text) as site_id,
+    cast(null as text) as device_mac_addr,
+    cast(null as text) as site_ref,
     cast(null as text) as created_at,
     cast(null as text) as updated_at
 where false
