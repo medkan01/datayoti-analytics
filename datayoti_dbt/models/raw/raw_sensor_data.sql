e42cdccb76e89dd32d0d205dbc16c0207aa6d515
medkan01 <mehdiakniou@gmail.com>
2025-10-30 01:19:32 +0100

    fix: Update device_id reference to device_mac_addr in raw_sensor_data model

--- datayoti_dbt/models/raw/raw_sensor_data.sql
+++ datayoti_dbt/models/raw/raw_sensor_data.sql
@@ -2,7 +2,7 @@
 
 select
     cast(null as text) as time,
-    cast(null as text) as device_id,
+    cast(null as text) as device_mac_addr,
     cast(null as text) as temperature,
     cast(null as text) as humidity,
     cast(null as text) as reception_time
