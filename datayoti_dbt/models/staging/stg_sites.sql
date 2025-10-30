cb9787f868f22535d345678c2c5a1bd3e9d633fe
medkan01 <mehdiakniou@gmail.com>
2025-10-30 01:18:49 +0100

    fix: Correct site_ref column reference in stg_sites SQL model

--- datayoti_dbt/models/staging/stg_sites.sql
+++ datayoti_dbt/models/staging/stg_sites.sql
@@ -1,11 +1,11 @@
 select
     cast(id as integer) as site_source_id,
-    cast(site_id as varchar(8)) as site_ref,
+    cast(site_ref as varchar(8)) as site_ref,
     cast(site_name as varchar(30)) as site_name,
     cast(description as varchar(255)) as site_description,
     cast(created_at as timestamp) as created_ts,
     cast(updated_at as timestamp) as updated_ts
 from {{ source('raw_iot', 'raw_sites') }}
 where id is not null
-and site_id is not null
+and site_ref is not null
 and created_at is not null
